5dcbd298a95ba9cdd1b59a1de9e94c9ac93b347c
abou961 <jean.aboumoussa@gmail.com>
2023-04-09 22:10:00 +0200

    🚧 Work in progress in the Mobile and Tablet Views

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ import Sidebar from './components/Sidebar';
 import SectionAbout from './components/SectionAbout';
 import SectionProjects from './components/SectionProjects';
 import SectionExperiences from './components/SectionExperiences';
+import { BrowserView, MobileView, TabletView } from 'react-device-detect';
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
-      <Introduction />
-      <SectionAbout />
-      <SectionProjects />
-      <SectionExperiences />
-      <Header />
+    <div>
+      <BrowserView>
+        <div className="App">
+          <Sidebar />
+          <Introduction />
+          <SectionAbout />
+          <SectionProjects />
+          <SectionExperiences />
+          <Header />
+        </div>
+      </BrowserView>
+      <MobileView>
+        <div className="App"> <h1>App not available on mobile and tablet.</h1></div>
+      </MobileView>
+      <TabletView>
+        <div className="App"> <h1>App not available on mobile and tablet.</h1></div>
+      </TabletView>
     </div>
   );
 }
